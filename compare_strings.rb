5b83de3f927fd50b3bee6792fac7886f011c4aa1
Chytoudis <c.chytoudis@acg.edu>
2014-04-08 11:17:30 +0300

    testing method for strings

--- compare_strings.rb
+++ compare_strings.rb
@@ -1,14 +1,21 @@
 #          /\ARuby/ 	Match "Ruby" at the start of a string
 
-def compare_strings(parameter)
+def compare_strings(text)
 
-  myString = parameter.to_s
+  property :myString, String, :length => 1024
+  #property :forms,    String, :length => 256
 
-  puts "Your " + parameter + " needs to be added in the header with data " unless myString.nil?
+  myString = text.to_s
+  teString = String.new(myString)
 
-   while !(myString.('\Z')) do myString.sub(/, /, d)  
+  myString.each_char {|c| print c, '' }
+
+  if myString.nil puts "Your " + parameter + " needs to be added in the header with data "
+
+   while !(myString.('\Z')) do myString.sub(/, /, 1)  
 
 	puts myString  
    
-   end	
+    end	
+  end
 end	
